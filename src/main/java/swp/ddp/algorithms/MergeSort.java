913986c23d9b4d1f142268f7b265af504e7136f9
David Samwald <if20b036@technikum-wien.at>
2021-06-22 20:09:03 +0200

    Added MergeSort and SelectionSort

--- src/main/java/swp/ddp/algorithms/MergeSort.java
+++ src/main/java/swp/ddp/algorithms/MergeSort.java
@@ -1,20 +1,161 @@
 package swp.ddp.algorithms;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class MergeSort<T extends Comparable<T>> implements SortingAlgorithm<T>{
+public class MergeSort<T extends Comparable<T>> implements SortingAlgorithm<T> {
     @Override
     public void sort(List<T> items) {
+
+        sortOfList(items, 0, items.size() - 1);
 
     }
 
     @Override
     public void sortInt(int[] items) {
 
+        sortIntArray(items, 0, items.length - 1);
+
     }
 
     @Override
     public String getName() {
         return "Merge Sort";
+    }
+
+    // Merges two sublists.
+    // First sublist is from index [l..m]
+    // Second sublist is from index [m+1..r]
+    void mergeOfList(List<T> items, int l, int m, int r) {
+        // Find sizes of two sublists to be merged
+        int n1 = m - l + 1;
+        int n2 = r - m;
+
+        /* Create temp Lists */
+        List<T> Left = new ArrayList<T>();
+        List<T> Right = new ArrayList<T>();
+
+        /*Copy data to temp Lists*/
+        for (int i = 0; i < n1; ++i)
+            Left.add(items.get(l+1));
+        for (int j = 0; j < n2; ++j)
+            Right.add(items.get(m+1+j));
+
+        /* Merge the temp Lists */
+
+        // Initial indexes of first and second sublist
+        int i = 0, j = 0;
+
+        // Initial index of merged sublist
+        int k = l;
+        while (i < n1 && j < n2) {
+
+            if (Left.get(i).compareTo(Right.get(j))<=0) {
+                items.set(k,Left.get(i));
+                i++;
+            } else {
+                items.set(k, Right.get(i));
+                j++;
+            }
+            k++;
+        }
+
+        /* Copy remaining elements of Left list if any */
+        while (i < n1) {
+            items.set(k, Left.get(i));
+            i++;
+            k++;
+        }
+
+        /* Copy remaining elements of Right list if any */
+        while (j < n2) {
+            items.set(k,Right.get(j));
+            j++;
+            k++;
+        }
+    }
+
+    // Main function that sorts Lisz from [l..r] using
+    // mergeOfList()
+    void sortOfList(List<T> items, int l, int r) {
+        if (l < r) {
+            // Find the middle point
+            int m = l + (r - l) / 2;
+
+            // Sort first and second halves
+            sortOfList(items, l, m);
+            sortOfList(items, m + 1, r);
+
+            // Merge the sorted halves
+            mergeOfList(items, l, m, r);
+        }
+    }
+
+    // Merges two subarrays of arr[].
+    // First subarray is arr[l..m]
+    // Second subarray is arr[m+1..r]
+    void mergeOfIntArray(int arr[], int l, int m, int r) {
+        // Find sizes of two subarrays to be merged
+        int n1 = m - l + 1;
+        int n2 = r - m;
+
+        /* Create temp arrays */
+        List<T> Left = new ArrayList<T>();
+        int L[] = new int[n1];
+        int R[] = new int[n2];
+
+        /*Copy data to temp arrays*/
+        for (int i = 0; i < n1; ++i)
+            L[i] = arr[l + i];
+        for (int j = 0; j < n2; ++j)
+            R[j] = arr[m + 1 + j];
+
+        /* Merge the temp arrays */
+
+        // Initial indexes of first and second subarrays
+        int i = 0, j = 0;
+
+        // Initial index of merged subarry array
+        int k = l;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        /* Copy remaining elements of L[] if any */
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        /* Copy remaining elements of R[] if any */
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+
+    // Main function that sorts arr[l..r] using
+    // mergeofIntArray()
+    void sortIntArray(int arr[], int l, int r) {
+        if (l < r) {
+            // Find the middle point
+            int m = l + (r - l) / 2;
+
+            // Sort first and second halves
+            sortIntArray(arr, l, m);
+            sortIntArray(arr, m + 1, r);
+
+            // Merge the sorted halves
+            mergeOfIntArray(arr, l, m, r);
+        }
     }
 }
--- src/main/java/swp/ddp/algorithms/SelectionSort.java
+++ src/main/java/swp/ddp/algorithms/SelectionSort.java
@@ -2,19 +2,63 @@ package swp.ddp.algorithms;
 
 import java.util.List;
 
-public class SelectionSort<T extends Comparable<T>> implements SortingAlgorithm<T>{
+public class SelectionSort<T extends Comparable<T>> implements SortingAlgorithm<T> {
     @Override
     public void sort(List<T> items) {
+
+        sortOfList(items);
 
     }
 
     @Override
     public void sortInt(int[] items) {
 
+        sortIntArray(items);
+
     }
 
     @Override
     public String getName() {
         return "Selection Sort";
+    }
+
+    void sortOfList(List<T> items) {
+        int n = items.size();
+
+        // One by one move boundary of unsorted sublist
+        for (int i = 0; i < n -1; i++){
+            // Find the minimum element in unsorted list
+            int min_idx = i;
+            for (int j = i+1; j < n; j++) {
+                if (items.get(j).compareTo(items.get(min_idx)) < 0) {
+                    min_idx = j;
+                }
+            }
+            // Swap the found minimum element with the first
+            // element
+            T temp = items.get(min_idx);
+            items.set(min_idx, items.get(i));
+            items.set(i,temp);
+        }
+    }
+
+    void sortIntArray(int items[]) {
+        int n = items.length;
+
+        // One by one move boundary of unsorted subarray
+        for (int i = 0; i < n - 1; i++) {
+            // Find the minimum element in unsorted array
+            int min_idx = i;
+            for (int j = i + 1; j < n; j++) {
+                if (items[j] < items[min_idx]) {
+                    min_idx = j;
+                }
+            }
+            // Swap the found minimum element with the first
+            // element
+            int temp = items[min_idx];
+            items[min_idx] = items[i];
+            items[i] = temp;
+        }
     }
 }
